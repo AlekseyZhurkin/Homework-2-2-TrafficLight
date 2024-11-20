4066b9c1be0013f58d482f988391f61a6b2466a1
Aleksey Zhurkin <web.zhurkin@gmail.com>
2024-11-20 13:22:44 +0300

    Initial Commit

---
+++ Homework-2-2-TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-2-2-TrafficLight
+//
+//  Created by Алексей Журкин on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
