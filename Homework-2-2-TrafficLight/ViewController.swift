23cc58996f7a7deb59f1ad54e8b0c4c8b65df934
Aleksey Zhurkin <web.zhurkin@gmail.com>
2024-11-20 18:16:57 +0300

    Light traffic app UI and logic

--- Homework-2-2-TrafficLight/ViewController.swift
+++ Homework-2-2-TrafficLight/ViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+    // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        
+        buttonLabel.setTitle("Start".uppercased(), for: .normal)
+        buttonLabel.layer.cornerRadius = 8
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func buttonDidTapped(_ sender: UIButton) {
+        buttonLabel.setTitle("Next".uppercased(), for: .normal)
+        
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            greenView.alpha = 0.3
+        } else if yellowView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            redView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+        } else {
+            redView.alpha = 1
+        }
+        
     }
 }
 
